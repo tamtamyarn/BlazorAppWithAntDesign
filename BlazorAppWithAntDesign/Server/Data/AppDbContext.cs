74be2319cf1008269140d0d684ad37780b550b7b
Yuki Nakayama <snow@macaroni.onmicrosoft.com>
2020-10-03 23:01:54 +0900

    プロジェクト ファイルを追加します。

---
+++ BlazorAppWithAntDesign/Server/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BlazorAppWithAntDesign.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorAppWithAntDesign.Server.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ BlazorAppWithAntDesign/Shared/Entities/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorAppWithAntDesign.Shared.Entities
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
